4c9f8b0c332edb3c8f3e9514310e3ed47a5f3d82
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-12 22:05:54 -0300

    chore: modificao do tipo de banco de dados e criacao do script de insercao do banco de dados

--- database/migrations/2025_09_05_230021_create_musicas_table.php
+++ database/migrations/2025_09_05_230021_create_musicas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nome');
             $table->string('cantor');
-            $table->datetime('ano_lancamento');
+            $table->year('ano_lancamento');
             $table->string('album');
             $table->timestamps();
         });
---
+++ script_inset_acervomusicapai.sql
@@ -0,0 +1,18 @@
+migrationsUSE acervomusicaapi;
+
+INSERT INTO musicas (nome, cantor, ano_lancamento, album, created_at, updated_at) VALUES
+('P do Pecado', 'Grupo Menos é Mais', '2025', 'P do Pecado', NOW(), NOW()),
+('Tubarões', 'Diego & Vitor Hugo', '2025', 'Ao Vivo Em Uberlândia Vol.02', NOW(), NOW()),
+('FAMOSINHA', 'Dj Caio Vieira', '2025', 'FAMOSINHA', NOW(), NOW()),
+('Pela Última Vez(Ao Vivo)', 'Grupo Menos é Mais', '2025', 'Pela Última Vez- Ao Vivo', NOW(), NOW()),
+('Ama Um Maloqueiro', 'Rafa e Junior', '2025', 'Ama Um Maloqueiro', NOW(), NOW()),
+('Eu Vou na Sua Casa', 'Felipe Amorim', '2025', 'RomantiDog', NOW(), NOW()),
+('Apaga Apaga Apaga- Ao Vivo', 'Danilo e Davi', '2024', 'Toma Essa Verdade(Ao Vivo)', NOW(), NOW()),
+('Coração Partido(Corazón Partío)- Ao Vivo', 'Grupo Menos é Mais', '2024', 'Churrasquinho 3(Ao Vivo)', NOW(), NOW()),
+('Última Saudade- Ao Vivo', 'Henrique & Juliano', '2024', 'Manifesto Musical 2(Ao Vivo/Vol.1)', NOW(), NOW()),
+('Baqueado- Ao Vivo', 'Panda', '2024', 'Panda Sem Moderação(Ao Vivo)', NOW(), NOW()),
+('Ilusão de Ótica- Ao Vivo', 'Matheus & Kauan', '2025', 'Envolvente(Ao Vivo/Vol.2', NOW(), NOW()),
+('Escondendo o Ouro- Ao Vivo', 'Zé NEto & Cristiano', '2025', 'Magia das Estrela, Vol.1(Ao Vivo)', NOW(), NOW()),
+('Grau de Maluca- Ao Vivo', 'Guilherme & Benuto', '2025', '+Amor, +Música Vol.3(Ao Vivo)', NOW(), NOW()),
+('Cópia Proibida', 'Léo Foguete', '2024', 'Obrigado Deus', NOW(), NOW()),
+('Caos de Alguem- Ao Vivo', 'Felipe e Rodrigo', '2025', 'Nossa Cara(Ao Vivo)', NOW(), NOW());
